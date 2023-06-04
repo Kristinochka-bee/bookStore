24d2211a9db094084c5817c62ba9eec79174419b
Kristina <111590366+Kristinochka-bee@users.noreply.github.com>
2023-06-04 15:12:51 +0200

    added GetAccountByAccountId need to finish

---
+++ src/test/java/api/schema/GetAccountByAccountIdDto.java
@@ -0,0 +1,19 @@
+package api.schema;
+
+import io.qameta.allure.internal.shadowed.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_DEFAULT)
+public class GetAccountByAccountIdDto {
+    private String userId;
+    private String username;
+    private String books;
+
+}
---
+++ src/test/java/tests/api/GetAccountTest.java
@@ -0,0 +1,13 @@
+package tests.api;
+
+import api.schema.GetAccountByAccountIdDto;
+import io.restassured.response.Response;
+
+
+public class GetAccountTest extends ApiBase{
+    int id;
+    GetAccountByAccountIdDto getAccountByAccountIdDto;
+    Response response;
+
+
+}

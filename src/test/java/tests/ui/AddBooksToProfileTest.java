a860c26dd1a9e20f14b7a2155bbb0207ac726d35
Kristina <111590366+Kristinochka-bee@users.noreply.github.com>
2023-06-12 14:15:45 +0200

    modified methods by using circles, need to fix

--- src/test/java/tests/ui/AddBooksToProfileTest.java
+++ src/test/java/tests/ui/AddBooksToProfileTest.java
@@ -3,10 +3,16 @@ package tests.ui;
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
 import e2e.pages.*;
+import org.openqa.selenium.JavascriptExecutor;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import tests.TestBase;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 
 
 public class AddBooksToProfileTest extends TestBase {
@@ -52,6 +58,28 @@ public class AddBooksToProfileTest extends TestBase {
         String actualAlertText = bookPage.getAlertText();
         Assert.assertEquals(actualAlertText, expectAlertText);
 
+        /*
+        String expectAlertText = "Book added to your collection.";
+        String actualAlertText = bookPage.getAlertText();
+
+        if (!expectAlertText.equals(actualAlertText)) {
+            homePage = new HomePage();
+            randomBook = homePage.getRandomBook();
+            bookTitle = randomBook.text();
+            randomBook.click();
+
+            bookPage.getAddToYourCollectionButton().shouldBe(Condition.visible);
+            bookPage.getBackToBookStoreButton().shouldBe(Condition.visible);
+            bookPage.getAddToYourCollectionButton().click();
+
+            expectAlertText = "Book added to your collection.";
+            actualAlertText = bookPage.getAlertText();
+        }
+
+        Assert.assertEquals(actualAlertText, expectAlertText);
+
+         */
+
         bookPage.clickAlertOkButton();
 
         leftMenu.getProfileMenuButton().click();
@@ -70,8 +98,9 @@ public class AddBooksToProfileTest extends TestBase {
 
     }
 
+    //TODO: в данном тесте было бы хорошо добавить цикл, что если книга уже присутствует , тогда добавляем другую рандомную книгу
     @Test
-    public void addAndDeleteSomeBooksInProfile(){
+    public void addAndDeleteSomeBooksInProfile() {
         homePage = new HomePage();
         bookPage = new BookPage();
 
@@ -80,17 +109,6 @@ public class AddBooksToProfileTest extends TestBase {
         leftMenu = new LeftMenu();
         leftMenu.clickOnbookStoreMenuButton();
 
-        for (int i = 0; i < homePage.getBooksTitles().size(); i++) {
-            if (i == 4 || i == 3 || i == 6){
-                homePage.getBooksTitles().get(i).click();
-                bookPage.getAddToYourCollectionButton().click();
-                bookPage.clickAlertOkButton();
-                bookPage.getBackToBookStoreButton().click();
-            }
-
-        }
-
-        /*
         homePage.getSearchBoxField().shouldBe(Condition.visible);
         homePage.getBooksTitles().findBy(Condition.exactText("You Don't Know JS"))
                 .shouldHave(Condition.href("/books?book=9781491904244")).click();
@@ -113,8 +131,6 @@ public class AddBooksToProfileTest extends TestBase {
         bookPage.getAddToYourCollectionButton().click();
         bookPage.clickAlertOkButton();
 
-         */
-
 
         leftMenu.getProfileMenuButton().click();
         profilePage = new ProfilePage();
@@ -136,4 +152,108 @@ public class AddBooksToProfileTest extends TestBase {
         profilePage.getDeleteAllBooksButton().click(); //удаляем все книги
 
     }
+
+    @Test
+    public void addAndDeleteSomeBooksInProfileCircle() {
+        homePage = new HomePage();
+        bookPage = new BookPage();
+
+        homePage.getLogOutButton().shouldBe(Condition.visible);
+
+        leftMenu = new LeftMenu();
+        leftMenu.clickOnbookStoreMenuButton();
+
+        for (int i = 0; i < homePage.getBooksTitles().size(); i++) {
+            if (i == 4 || i == 3 || i == 6) {
+                homePage.getBooksTitles().get(i).click();
+                bookPage.getAddToYourCollectionButton().click();
+                bookPage.clickAlertOkButton();
+                bookPage.getBackToBookStoreButton().click();
+            }
+
+        }
+
+        leftMenu.getProfileMenuButton().click();
+        profilePage = new ProfilePage();
+        profilePage.getTableRow().shouldBe(Condition.visible);
+        profilePage.getYouDontKnowJSBook().shouldBe(Condition.visible);// книги отображаются в таблице
+        profilePage.getSpeakingJavaScriptBook().shouldBe(Condition.visible);
+        profilePage.getSpeakingJavaScriptBook().isDisplayed();
+        profilePage.getSearchBox().sendKeys("You Don't Know JS");
+        //profilePage.getTable().shouldHave(Condition.exactText("You Don't Know JS"));
+        profilePage.getDeleteIcon().click();
+
+        popUpWindow = new PopUpWindow();
+        popUpWindow.getWindowTitle().shouldBe(Condition.visible);
+        popUpWindow.getOkButton().click();
+
+        profilePage.getSearchBox().sendKeys("You Don't Know JS");
+        profilePage.getTable().shouldBe(Condition.exactText(" ")); // проверкa на то,что книга удалилась
+
+        profilePage.getDeleteAllBooksButton().click(); //удаляем все книги
+
+    }
+
+    //TODO : книги не добавились в профайл, соответственно проверка на отображение книг в профайле не проходит
+
+    @Test
+    public void addAndDeleteSomeBooksInProfileCircle1() {
+        homePage = new HomePage();
+        bookPage = new BookPage();
+
+        homePage.getLogOutButton().shouldBe(Condition.visible);
+
+        leftMenu = new LeftMenu();
+        leftMenu.clickOnbookStoreMenuButton();
+
+        homePage.getSearchBoxField().shouldBe(Condition.visible);
+
+        // Список книг для добавления
+        List<String> booksToAdd = Arrays.asList(
+                "You Don't Know JS",
+                "Speaking JavaScript",
+                "Eloquent JavaScript, Second Edition"
+        );
+
+        for (String bookTitle : booksToAdd) {
+            SelenideElement book = homePage.getBooksTitles().findBy(Condition.exactText(bookTitle));
+            String bookHref = book.getAttribute("href");
+            book.click();
+
+            bookPage.getBackToBookStoreButton().shouldBe(Condition.visible);
+            bookPage.getAddToYourCollectionButton().click();
+            JavascriptExecutor js = (JavascriptExecutor) getWebDriver();
+            js.executeScript("alert('Привет, это алерт!');");
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            org.openqa.selenium.Alert alert = getWebDriver().switchTo().alert();
+            js.executeScript("arguments[0].accept();", alert);
+
+            //bookPage.clickAlertOkButton();
+            bookPage.getBackToBookStoreButton().click();
+        }
+
+        leftMenu.getProfileMenuButton().click();
+        profilePage = new ProfilePage();
+        profilePage.getTableRow().shouldBe(Condition.visible);
+        profilePage.getYouDontKnowJSBook().shouldBe(Condition.visible);
+        profilePage.getSpeakingJavaScriptBook().shouldBe(Condition.visible);
+        profilePage.getSpeakingJavaScriptBook().isDisplayed();
+        profilePage.getSearchBox().sendKeys("You Don't Know JS");
+
+        profilePage.getDeleteIcon().click();
+
+        popUpWindow = new PopUpWindow();
+        popUpWindow.getWindowTitle().shouldBe(Condition.visible);
+        popUpWindow.getOkButton().click();
+
+        profilePage.getSearchBox().sendKeys("You Don't Know JS");
+        profilePage.getTable().shouldBe(Condition.exactText(" "));
+
+        profilePage.getDeleteAllBooksButton().click();
+    }
 }
+//TODO: не нажимается кнопка OK аллерт
